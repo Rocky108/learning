10c52af5216f7174ce529d93cb18d865f590a774
Rocky108 <dougald2001@gmail.com>
2019-09-30 09:02:13 -0500

    maths

--- learning.py
+++ learning.py
@@ -1,4 +1,13 @@
-print("hello world")
+# this is an octothorpe - it makes a comment (is ignored by the computer)
+# print("hello world")
 print("howdy, y'all")
 print("I like typing this.")
 print("This is fun.")
+
+# math skills demo
+
+print("I will not count my chickens", 2)
+print("Hens: ", 25+30/6)
+print("Roosters: ", 100 - 25 * 3 % 4)
+print("Now i will count the eggs", 7)
+print(3+2+1-5+4%2-1/4+6)
